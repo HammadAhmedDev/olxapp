bb4e0629fb6767b1f7d63568b630c3d0000a8212
HammadAhmedSherazi <ahmedhammad262@gmail.com>
2021-04-04 00:47:39 -0700

    olx app

--- src/comp/SubInfo.jsx
+++ src/comp/SubInfo.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
-
-
+import facebookIcon from './images/facebook icon.png';
+import twitterIcon from './images/twitter icon.png';
+import instaIcon from './images/insta icon.png';
+import playIcon from './images/playicon.png'
 export default function SubInfoBox(){
     return(
-        <Row>
+        <Row className='SubInfoContainer'>
             <table className='SubInfoContainer'>
                 <tr className='tableHeading'>
                     <th>POPULAR CATEGORIES</th>
@@ -18,22 +20,29 @@ export default function SubInfoBox(){
                     <td><a href="#Bikes">Bikes</a></td>
                     <td><a href="#AboutOlxGroups">About Olx Group</a></td>
                     <td><a href="Help">Help</a></td>
-                    <td className="socialLink">
-                        <ul>
+                    <td rowSpan="2">
+                        <ul className="socialLink">
                             <li>
-                               <input type="image" src="src\comp\images\facebook icon.png" alt=""/>
+                               <input type="image" src={facebookIcon} width='1px' alt=""/>
                             </li>
-                            <li></li>
-                            <li></li>
-                            <li>    </li>
+                            <li>
+                               <input type="image" src={twitterIcon} alt=""/>
+                            </li>
+                            <li>
+                               <input type="image" src={playIcon} alt=""/>
+                            </li>
+                            <li>
+                               <input type="image" src={instaIcon} alt=""/>
+                            </li>
                         </ul>
                     </td>
                 </tr>
                 <tr className='linkInfo'>
+                
+                    <td>Flats for rent</td>
                     <td>Watches</td>
                     <td>OLX Blog</td>
                     <td>Sitemap</td>
-                    <td></td>
                     <td></td>
                 </tr>
                 <tr className='linkInfo'>
@@ -41,12 +50,12 @@ export default function SubInfoBox(){
                     <td>Books</td>
                     <td>Contact Us </td>
                     <td>Legal & Privacy information</td>
-                    <td></td>
+                    <td rowSpan='2'></td>
                 </tr>
                 <tr className='linkInfo'>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>Mobile Phones</td>
+                    <td>Dogs</td>
+                    <td>OLX for Businesses</td>
                     <td></td>
                     <td></td>
                 </tr>
